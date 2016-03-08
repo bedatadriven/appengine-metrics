033ec3659839094af7d628b57f909f7c6eb8fd6c
Alex Bertram <alex@bedatadriven.com>
2016-03-08 11:57:25 +0100

    Ensured that malformed messages do not crash the aggregator thread

--- statsd/src/main/java/com/bedatadriven/appengine/metrics/Aggregator.java
+++ statsd/src/main/java/com/bedatadriven/appengine/metrics/Aggregator.java
@@ -6,49 +6,52 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Background thread which process the queue of incoming messages 
  * sequentially, updating the statistics as it goes.
- * 
+ *
  */
 class Aggregator implements Runnable {
 
     private static final Logger LOGGER = Logger.getLogger(Aggregator.class.getName());
 
-    public static final ArrayBlockingQueue<String> MESSAGE_QUEUE = new ArrayBlockingQueue<>(5000);
-    
+    public static final ArrayBlockingQueue<String> MESSAGE_QUEUE = new ArrayBlockingQueue<>(50000);
+
     private Map<String, TimingStatistic> timers = Maps.newHashMap();
     private Map<String, CountStatistic> counts = Maps.newHashMap();
 
     private long lastFlushTime = 0;
 
     private long flushInterval = TimeUnit.MINUTES.toMillis(1);
-    
+
     @Override
     public void run() {
-
-
         while(true) {
             String message;
             try {
                 message = MESSAGE_QUEUE.poll(timeUntilNextFlush(), TimeUnit.MILLISECONDS);
+
+
+                LOGGER.fine("Polled: " + message);
+
+
+                if(message != null) {
+                    processMessage(message);
+                }
+
+                long timeSinceLastFlush = System.currentTimeMillis() - lastFlushTime;
+                if(timeSinceLastFlush > flushInterval) {
+                    flush();
+                }
+                
             } catch (InterruptedException e) {
                 LOGGER.severe("Aggregator interrupted.");
                 return;
-            }
-            
-            LOGGER.fine("Polled: " + message);                    
-
-
-            if(message != null) {
-                processMessage(message);
-            }
-
-            long timeSinceLastFlush = System.currentTimeMillis() - lastFlushTime;
-            if(timeSinceLastFlush > flushInterval) {
-                flush();
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "Exception processing queue: " + e.getMessage(), e);
             }
         }
     }
@@ -58,12 +61,12 @@ class Aggregator implements Runnable {
         if(timeSinceLastFlush > flushInterval) {
             return 0;
         }
-        
+
         return flushInterval - timeSinceLastFlush;
     }
 
     void processMessage(String message) {
-        
+
         String messageParts[];
         try {
             messageParts = Protocol.parse(message);
@@ -77,7 +80,7 @@ class Aggregator implements Runnable {
             addTiming(messageParts[0], messageParts[1]);
         } else if(type.equals(Protocol.COUNT)) {
             addCount(messageParts[0], messageParts[1]);
-            
+
         } else {
             LOGGER.severe("Unknown message type:" + type);
         }
@@ -86,7 +89,7 @@ class Aggregator implements Runnable {
 
     private void addTiming(String namespace, String millisecondsString) {
         int ms = Integer.parseInt(millisecondsString);
-        
+
         TimingStatistic histogram = timers.get(namespace);
         if(histogram == null) {
             histogram = new TimingStatistic(namespace);
@@ -97,7 +100,7 @@ class Aggregator implements Runnable {
 
 
     private void addCount(String namespace, String countString) {
-        
+
         CountStatistic count = counts.get(namespace);
         if(count == null) {
             count = new CountStatistic(namespace);
@@ -113,14 +116,14 @@ class Aggregator implements Runnable {
         // Create a new snapshot object with the current state
         // of all our statistics
         Snapshot snapshot = new Snapshot(timers.values(), counts.values());
-        
-        
+
+
         // Reset our statistics
         this.timers = new HashMap<>();
         this.counts = new HashMap<>();
-        
+
         lastFlushTime = System.currentTimeMillis();
-        
+
         // Now hand of the snapshot to the reporting thread
         Reporter.FLUSH_QUEUE.add(snapshot);
     }
